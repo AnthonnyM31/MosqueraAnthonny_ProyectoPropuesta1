877c4d6af475fda28175752a1933a19ce2dc0dea
AnthonnyM31 <anthonnymosquera1492@gmail.com>
2024-11-03 13:22:22 -0500

    Agregar archivos de proyecto.

---
+++ MosqueraAnthonny_ProyectoPropuesta1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MosqueraAnthonny_ProyectoPropuesta1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
